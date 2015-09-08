1627166afc43a11faf251155ff4b1ca6318fc68f
Anthony Nevo <anthony.nevo@gmail.com>
2015-09-08 13:50:33 +0200

    Changed system.out

--- src/main/java/yose/Yose.java
+++ src/main/java/yose/Yose.java
@@ -41,6 +41,6 @@ public class Yose {
     public static void main(String[] args) throws IOException {
         Yose yose = new Yose(port(args));
         yose.start();
-        System.out.print("To play the game visit " + yose.uri());
+        System.out.print("To test the game visit " + yose.uri());
     }
 }
